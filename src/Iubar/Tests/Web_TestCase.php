cd1e5efb5f2609bc5bc4b41860f550fda54cc575
Matteo Mirisola <mirimmm94@gmail.com>
2016-07-06 18:23:04 +0200

    .

--- src/Iubar/Tests/Web_TestCase.php
+++ src/Iubar/Tests/Web_TestCase.php
@@ -137,7 +137,7 @@ class Web_TestCase extends Root_TestCase {
                     $def_browser_version = "46.0";
                     break;
                 case self::FIREFOX:
-                    $def_browser_version = "39.0";
+                    $def_browser_version = "44.0";
                     break;
                 case self::MARIONETTE:
                     $def_browser_version = "39.0";
