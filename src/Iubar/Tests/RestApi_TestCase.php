6b1249f5e101abb6cd0a1fbdc0b601b1b761f453
danielemontesi <daniele.montesi@gmail.com>
2016-08-18 19:36:02 +0200

    .

--- src/Iubar/Tests/RestApi_TestCase.php
+++ src/Iubar/Tests/RestApi_TestCase.php
@@ -12,6 +12,13 @@ use GuzzleHttp\Exception\RequestException;
  */
 class RestApi_TestCase extends Root_TestCase {
 
+    const APP_JSON_CT = 'application/json';
+    
+    const HTTP_OK = 200;
+    
+    const CONTENT_TYPE = 'Content-Type';
+    
+    
     /**
      * Handle the RequestException writing his msg
      *
@@ -20,5 +27,56 @@ class RestApi_TestCase extends Root_TestCase {
     protected function handleException(RequestException $e) {
         echo "REQUEST: " . Psr7\str($e->getRequest());
         echo "ECCEZIONE: " . $e->getMessage() . PHP_EOL;
+        $this->fail();
     }
+    
+    protected function sleep($seconds){
+        self::$climate->info('Waiting ' . $seconds . ' seconds...');
+        sleep($seconds);
+    }
+    
+    /**
+     * Send an http request and return the response
+     *
+     * @param string $method the method
+     * @param string $partial_uri the partial uri
+     * @param string $array the query
+     * @param int $timeout the timeout
+     * @return string the response
+     */
+    protected function sendRequest($method, $partial_uri, $array, $timeout) {
+        $response = null;
+        try {
+            $request = new Request($method, $partial_uri);
+            $response = $this->client->send($request, [
+                'timeout' => $timeout,
+                'query' => $array
+            ]);
+        } catch (RequestException $e) {
+            $this->handleException($e);
+        }
+        return $response;
+    }
+    
+    /**
+     * Check the OK status code and the APP_JSON_CT content type of the response
+     *
+     * @param string $response the response
+     * @return string the body of the decode response
+     */
+    protected function checkResponse($response) {
+        $data = null;
+        if ($response) {
+            // Response
+            $this->assertContains(self::APP_JSON_CT, $response->getHeader(self::CONTENT_TYPE)[0]);
+            $this->assertEquals(self::HTTP_OK, $response->getStatusCode());
+    
+            self::$climate->shout('Response code is: ' . $response->getStatusCode());
+            // Getting data
+            $data = json_decode($response->getBody(), true);
+        }
+        return $data;
+    }
+    
+    
 }
