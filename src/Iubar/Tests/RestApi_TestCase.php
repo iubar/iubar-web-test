7c4c382ee4b45a59b44997c8b665a877cd4409ad
danielemontesi <daniele.montesi@iubar.it>
2016-12-14 12:11:22 +0100

    .
    .

--- src/Iubar/Tests/RestApi_TestCase.php
+++ src/Iubar/Tests/RestApi_TestCase.php
@@ -36,7 +36,7 @@ abstract class RestApi_TestCase extends Root_TestCase {
         
     const CONTENT_TYPE = 'Content-Type';
         
-    const TIMEOUT = 4; // seconds
+    const TIMEOUT = 6; // seconds
       
     protected static $client = null;
 
